effadc6cf85c1c0a5c1e1de664fcbe92e495db65
nidhinPc <nidhinpc66666@gmail.com>
2024-11-07 09:52:40 +0530

    razorpay

--- lib/view/my_cart/my_cart.dart
+++ lib/view/my_cart/my_cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:shoppingcardui/controller/my_cart_controller.dart';
 
 class MyCart extends StatefulWidget {
@@ -193,28 +194,85 @@ class _MyCartState extends State<MyCart> {
                     ),
                   ],
                 ),
-                Container(
-                  height: 40,
-                  width: 160,
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Checkout",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.shopping_cart_outlined,
-                        color: Colors.white,
-                        size: 18,
-                      )
-                    ],
+                InkWell(
+                  onTap: () {
+                    Razorpay razorpay = Razorpay();
+                    var options = {
+                      'key': 'rzp_test_1DP5mmOlF5G5ag',
+                      'amount': 100,
+                      'name': 'Acme Corp.',
+                      'description': 'Fine T-Shirt',
+                      'retry': {'enabled': false, 'max_count': 1},
+                      'send_sms_hash': true,
+                      'prefill': {
+                        'contact': '8888888888',
+                        'email': 'test@razorpay.com'
+                      },
+                      'external': {
+                        'wallets': ['paytm']
+                      }
+                    };
+                    razorpay.on(
+                      Razorpay.EVENT_PAYMENT_ERROR,
+                      handlePaymentErrorResponse,
+                    );
+                    razorpay.on(
+                      Razorpay.EVENT_PAYMENT_SUCCESS,
+                      handlePaymentSuccessResponse,
+                    );
+                    razorpay.on(
+                      Razorpay.EVENT_EXTERNAL_WALLET,
+                      handleExternalWalletSelected,
+                    );
+                    razorpay.open(options);
+
+                    // Razorpay razorpay = Razorpay();
+                    // var options = {
+                    //   'key': 'rzp_live_ILgsfZCZoFIKMb',
+                    //   'amount': 100,
+                    //   'name': 'Acme Corp.',
+                    //   'description': 'Fine T-Shirt',
+                    //   'retry': {'enabled': true, 'max_count': 1},
+                    //   'send_sms_hash': true,
+                    //   'prefill': {
+                    //     'contact': '8888888888',
+                    //     'email': 'test@razorpay.com'
+                    //   },
+                    //   'external': {
+                    //     'wallets': ['paytm']
+                    //   }
+                    // };
+                    // razorpay.on(Razorpay.EVENT_PAYMENT_ERROR,
+                    //     handlePaymentErrorResponse);
+                    // razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS,
+                    //     handlePaymentSuccessResponse);
+                    // razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET,
+                    //     handleExternalWalletSelected);
+                    // razorpay.open(options);
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 160,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Checkout",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.white,
+                          size: 18,
+                        )
+                      ],
+                    ),
                   ),
                 )
               ],
@@ -222,4 +280,103 @@ class _MyCartState extends State<MyCart> {
           )),
     );
   }
+
+  void handlePaymentErrorResponse(PaymentFailureResponse response) {
+    // PaymentFailureResponse contains three values:
+    // 1. Error Code
+    // 2. Error Description
+    // 3. Metadata
+    showAlertDialog(
+      context,
+      'Payment Failed',
+      'Code: ${response.code}\n'
+          'Description: ${response.message}\n'
+          'Metadata: ${response.error.toString()}',
+    );
+  }
+
+  void handlePaymentSuccessResponse(PaymentSuccessResponse response) {
+    // PaymentSuccessResponse contains three values:
+    // 1. Order ID
+    // 2. Payment ID
+    // 3. Signature
+    showAlertDialog(
+      context,
+      'Payment Successful',
+      'Payment ID: ${response.paymentId}',
+    );
+  }
+
+  void handleExternalWalletSelected(ExternalWalletResponse response) {
+    showAlertDialog(
+      context,
+      'External Wallet Selected',
+      '${response.walletName}',
+    );
+  }
+
+  void showAlertDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            ElevatedButton(
+              onPressed: Navigator.of(context).pop,
+              child: const Text('Continue'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // void handlePaymentErrorResponse(PaymentFailureResponse response) {
+  //   /*
+  //   * PaymentFailureResponse contains three values:
+  //   * 1. Error Code
+  //   * 2. Error Description
+  //   * 3. Metadata
+  //   * */
+  //   showAlertDialog(context, "Payment Failed",
+  //       "Code: ${response.code}\nDescription: ${response.message}\nMetadata:${response.error.toString()}");
+  // }
+
+  // void handlePaymentSuccessResponse(PaymentSuccessResponse response) {
+  //   /*
+  //   * Payment Success Response contains three values:
+  //   * 1. Order ID
+  //   * 2. Payment ID
+  //   * 3. Signature
+  //   * */
+  //   showAlertDialog(
+  //       context, "Payment Successful", "Payment ID: ${response.paymentId}");
+  // }
+
+  // void handleExternalWalletSelected(ExternalWalletResponse response) {
+  //   showAlertDialog(
+  //       context, "External Wallet Selected", "${response.walletName}");
+  // }
+
+  // void showAlertDialog(BuildContext context, String title, String message) {
+  //   // set up the buttons
+  //   Widget continueButton = ElevatedButton(
+  //     child: const Text("Continue"),
+  //     onPressed: () {},
+  //   );
+  //   // set up the AlertDialog
+  //   AlertDialog alert = AlertDialog(
+  //     title: Text(title),
+  //     content: Text(message),
+  //   );
+  //   // show the dialog
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return alert;
+  //     },
+  //   );
+  // }
 }
